e66d3de16f6e918d5758887439bd0667b67ebd4a
sanath <ramsaanath@gmail.com>
2019-09-10 21:48:22 +0530

    Remove unnecessary comments

--- src/router.ts
+++ src/router.ts
@@ -4,11 +4,9 @@ import errorHandlers from './middlewares/error-handler';
 import routes from './routes';
 import { applyMiddlewares, applyRoutes } from './utils';
 
-// setup routes, middlewares and error handlers
 const router = express();
 applyMiddlewares(middlewares, router);
 applyRoutes(routes, router, '/api/v1');
 applyMiddlewares(errorHandlers, router);
 
-// export the router
 export default router;
