cd3c2adfd79fcfd339b8ece9b7842f95e9683b3f
sanath <ram@solarforschools.co>
2020-08-02 17:00:20 +0530

    quick fix for donor registration bug

--- src/service/donor-service.ts
+++ src/service/donor-service.ts
@@ -6,6 +6,9 @@ export const createDonor = async (donor: Donor, userId: number) => {
     const user = await User.findOne(userId);
     donor.user = user!;
     donor.residence = await getPlace(donor.residence.gmapsId);
+    if (donor.available == null || donor.available == undefined) {
+        donor.available = true;
+    }
     await donor.save();
 };
 
